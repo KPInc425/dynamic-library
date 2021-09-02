825c98c8bec750d37f6e8a435f00dcc3e463611b
KPInc425 <KPInc425@gmail.com>
2021-09-02 02:00:09 -0700

    Fixed remove Book button, Added ChangeReadStatus Button, Added form for input of new book rather than prompts.

--- library.js
+++ library.js
@@ -1,3 +1,9 @@
+if (storageAvailable('localStorage')) {
+    console.log("Local Storage is Available");
+} else {
+    console.log("No Local Storage Available.");
+}
+
 let myLibrary = [
     {
         author: "Jim Mortimore",
@@ -28,17 +34,14 @@ let myLibrary = [
 // Ref main library container
 const libraryContainer = document.querySelector('#cardContainer');
 displayLibraryBooks(myLibrary);
-addNewBook(myLibrary);
+// addNewBook(myLibrary);
 addEventListenerRemoveButton(myLibrary);
-
+addEventListenerChangeReadStatus(myLibrary);
+displayAddBookForm(myLibrary);
 
 
 function Book() {
     // the constructor
-}
-
-function addBookToLibrary() {
-    // do stuff here
 }
 
 function displayLibraryBooks(library) {
@@ -54,6 +57,7 @@ function displayLibraryBooks(library) {
                         \r\n?Read?\r\n ${book.read}`;
         const bookCard = document.createElement('div');
         bookCard.classList.add("card");
+        bookCard.classList.add("cardWidth");
         bookCard.setAttribute("data-index", i);
         const bookCardInfo = document.createElement('div');
         bookCardInfo.classList.add("bookInfoContainer");
@@ -63,53 +67,108 @@ function displayLibraryBooks(library) {
         bookCardRemoveButton.classList.add("buttonRemoveBook");
         bookCardRemoveButton.setAttribute("value", "Remove Book");
         bookCardRemoveButton.textContent = "Remove Book";
-        bookCardRemoveButton.setAttribute("data-index", i);
+        // add button to change READ status of book 
+        const bookCardChangeReadStatusButton = document.createElement('button');
+        bookCardChangeReadStatusButton.classList.add("buttonChangeReadStatus");
+        bookCardChangeReadStatusButton.setAttribute("value", "Change Read Status");
+        bookCardChangeReadStatusButton.textContent = "Change Read Status";
+        // Added this to make the changeReadStatusButton work, it seems it wouldn't 
+        // read the data-* from the bookCard like the remove button would.
+        bookCardChangeReadStatusButton.setAttribute("data-index", i);
+
+        
 
 
         bookCard.appendChild(bookCardInfo);
         bookCard.appendChild(bookCardRemoveButton);
+        bookCard.appendChild(bookCardChangeReadStatusButton);
         libraryContainer.appendChild(bookCard);
         i++;
     }
 }
 
+function displayAddBookForm(library) {
+    // ref the newBook button
+    document.getElementById('newBook').onclick = function() {
+        // alert("click");
+        const newBookForm = document.getElementById('newBookForm');
+        newBookForm.style.display = "block";
+        // alert(newBookForm.style.display)
+        addNewBook(library);
+    }
+}
+
 function addNewBook(library) {
-    document.getElementById("newBook").onclick = function() {
-        // I should likely rewrite this into a form that will show when clicked.
-        alert("You are about to input a new Book into the Library.")
-        newBookAuthor = prompt("Who is the Author?");
-        if (newBookAuthor == "" || newBookAuthor == null) {
-            // You can just click okay again, need to find recourse for this
-            prompt("Who is the Author?");
+    document.querySelector(".addBookButton").onclick = function() {
+        const inputAuthor = document.querySelector("#Author").value;
+        const inputTitle = document.querySelector("#Title").value;
+        const inputPages = document.querySelector("#Pages").value;
+        const inputRead = document.querySelector("#Read").value;
+        console.log(`${inputAuthor} ${inputTitle} ${inputPages} ${inputRead}`)
+        alert(`${inputAuthor} ${inputTitle} ${inputPages} ${inputRead}`)
+        const newBook = {
+            author: inputAuthor,
+            title: inputTitle,
+            pages: inputPages,
+            read: inputRead,
         }
-        newBookTitle = prompt("What is the Title of New Book?");
-        if (newBookAuthor == "" || newBookAuthor == null) {
-            prompt("What is the Title of New Book?");
-        }
-        newBookPages = prompt("How many pages?");
-        newBookPages = Number(newBookPages);
-        if (typeof newBookPages !=  'number') {
-            prompt("How many Pages?")
-        }
-        newBookRead = Boolean(prompt("Has the new book been read? true or false."));
-        // This doesn't quite work either
-        if (typeof newBookReady != 'boolean') {
-            prompt("Has the new book been read?");
-        }
-        console.log(newBookAuthor + " " + newBookTitle + " " + newBookPages + " " + newBookRead)
-        newBook = {
-            author: newBookAuthor,
-            title: newBookTitle,
-            pages: newBookPages,
-            read: newBookRead,
-        }
+        // Reset text Boxes
+        document.querySelector("#Author").value = "";
+        document.querySelector("#Title").value = "";
+        document.querySelector("#Pages").value = "";
+        document.querySelector("#Read").value = "";
+        // Hide form again
+        const newBookForm = document.getElementById('newBookForm');
+        newBookForm.style.display = "none";
+        // add new book
         library.push(newBook);
-        // Remove library html to replace all cards rather than append
+        // clear space for new library
         libraryContainer.innerHTML = "";
         displayLibraryBooks(library);
         addEventListenerRemoveButton(library);
+        addEventListenerChangeReadStatus(library);
+
     };
 }
+
+// function addNewBook(library) {
+//     document.getElementById("newBook").onclick = function() {
+//         // I should likely rewrite this into a form that will show when clicked.
+//         alert("You are about to input a new Book into the Library.")
+//         newBookAuthor = prompt("Who is the Author?");
+//         if (newBookAuthor == "" || newBookAuthor == null) {
+//             // You can just click okay again, need to find recourse for this
+//             prompt("Who is the Author?");
+//         }
+//         newBookTitle = prompt("What is the Title of New Book?");
+//         if (newBookAuthor == "" || newBookAuthor == null) {
+//             prompt("What is the Title of New Book?");
+//         }
+//         newBookPages = prompt("How many pages?");
+//         newBookPages = Number(newBookPages);
+//         if (typeof newBookPages !=  'number') {
+//             prompt("How many Pages?")
+//         }
+//         newBookRead = Boolean(prompt("Has the new book been read? true or false."));
+//         // This doesn't quite work either
+//         if (typeof newBookReady != 'boolean') {
+//             prompt("Has the new book been read?");
+//         }
+//         console.log(newBookAuthor + " " + newBookTitle + " " + newBookPages + " " + newBookRead)
+//         newBook = {
+//             author: newBookAuthor,
+//             title: newBookTitle,
+//             pages: newBookPages,
+//             read: newBookRead,
+//         }
+//         library.push(newBook);
+//         // Remove library html to replace all cards rather than append
+//         libraryContainer.innerHTML = "";
+//         displayLibraryBooks(library);
+//         addEventListenerRemoveButton(library);
+//         addEventListenerChangeReadStatus(library);
+//     };
+// }
 
 function removeBookFromLibrary(library, index) {
     // Remove object from array
@@ -117,6 +176,7 @@ function removeBookFromLibrary(library, index) {
     libraryContainer.innerHTML = "";
     displayLibraryBooks(library);
     addEventListenerRemoveButton(library);
+    addEventListenerChangeReadStatus(library);
     
 }
 
@@ -130,4 +190,58 @@ function addEventListenerRemoveButton(library) {
             removeBookFromLibrary(library, index); 
         })
     })
+}
+
+function addEventListenerChangeReadStatus(library) {
+    const buttonChangeReadStatus = document.querySelectorAll(".buttonChangeReadStatus");
+    buttonChangeReadStatus.forEach((div) => {
+        // Click to get index and change READ status
+        div.addEventListener("click", function(){
+            // get Index from data-* 
+            // alert("TESTING");
+            let index = div.getAttribute('data-index');
+            // alert(index);
+            changeBookReadStatus(library, index);
+        })
+    })
+}
+
+function changeBookReadStatus(library, index) {
+    // Change Book READ status 
+    // alert(index);
+    if (library[index].read == true) {
+        library[index].read = false;
+    } else {
+        library[index].read = true;
+    }
+    libraryContainer.innerHTML = "";
+    displayLibraryBooks(library);
+    addEventListenerChangeReadStatus(library);
+    addEventListenerRemoveButton(library);
+    
+}
+
+function storageAvailable(type) {
+    var storage;
+    try {
+        storage = window[type];
+        var x = '__storage_test__';
+        storage.setItem(x,x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch(e) {
+        return e instanceof DOMException && (
+            // everthing except firefox 
+            e.code === 22 || 
+            // firefox 
+            e.code === 1014 || 
+            // test name field too, because code might not be present 
+            // everything except Firefox 
+            e.name === 'QuotoExceededError' ||
+            // firefox 
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && 
+            // Acknoledge QuotaExceededError only if there's somethign alread stored 
+            (storage && storage.length !== 0);
+    }
 }
